93cca1ff2ea06726edcf06107153f4a14e427aa1
Mihir Paldhikar <imihirpaldhikar@gmail.com>
2023-02-28 01:43:00 +0530

    Added Caching Headers support.

--- src/main/kotlin/com/mihirpaldhikar/plugins/HTTP.kt
+++ src/main/kotlin/com/mihirpaldhikar/plugins/HTTP.kt
@@ -35,7 +35,7 @@ fun Application.configureHTTP() {
     install(CachingHeaders) {
         options { _, outgoingContent ->
             when (outgoingContent.contentType?.withoutParameters()) {
-                ContentType.Text.CSS -> CachingOptions(CacheControl.MaxAge(maxAgeSeconds = 24 * 60 * 60))
+                ContentType.Text.Any -> CachingOptions(CacheControl.MaxAge(maxAgeSeconds = 24 * 60 * 60))
                 else -> null
             }
         }
